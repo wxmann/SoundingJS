c2f8ae8f8648815f071aa5898c423e5191df4a1b
wxmann <wxfreakk91@gmail.com>
2016-04-28 00:51:40 -0700

    Move files to plot folder, split plot.js up for better code readability.

---
+++ js/plot/plot-common.js
@@ -0,0 +1,93 @@
+/**
+ * Created by tangz on 4/28/2016.
+ */
+
+var SVG_NS = 'http://www.w3.org/2000/svg';
+
+var Elements = {
+    SKEW_T_BOUNDARY: "skewTBoundary",
+    TEMP_TRACE: "tempTrace",
+    DEWPT_TRACE: "dewptTrace",
+    SB_PARCEL_TRACE: "sbParcelTrace",
+    ISOTHERMS: "isotherms",
+    ISOBARS: "isobars",
+    MIXING_RATIOS: "mixingRatios",
+    DRY_ADIABATS: "dryAdiabats",
+    MOIST_ADIABATS: "moistAdiabats",
+    WIND_BARBS: "windBarbs",
+    ISOBAR_LABELS: "pLabels",
+    ISOTHERM_LABELS: "tLables",
+
+    HODO_BOUNDARY: "hodographBoundary",
+    WINDSPEED_RADII: "windRadii",
+    HODO_AXES: "hodoAxes",
+    // HODO_TRACE: "hodoTrace",
+
+    hodoTrace: {
+        KM_0_3: "Hodo_km_0_3",
+        KM_3_6: "Hodo_km_3_6",
+        KM_6_9: "Hodo_km_6_9",
+        KM_gt_9: "Hodo_km_gt_9"
+    },
+
+    classes: {
+        BOUNDARY: "bdy",
+        TRACE: "trace",
+        KM_0_3: "km_0_3",
+        KM_3_6: "km_3_6",
+        KM_6_9: "km_6_9",
+        KM_gt_9: "km_gt_9"
+    }
+};
+
+var createBoundaryRect = function(id, hgt, width) {
+    var rect = document.createElementNS(SVG_NS, 'rect');
+    rect.id = id;
+    rect.setAttribute('class', Elements.classes.BOUNDARY);
+    rect.setAttribute('height', hgt.toString());
+    rect.setAttribute('width', width.toString());
+    return rect;
+};
+
+var createGroupElement = function (id) {
+    var groupElem = document.createElementNS(SVG_NS, 'g');
+    groupElem.id = id;
+    return groupElem;
+};
+
+var getPath = function(coords) {
+    var allpoints = [];
+    var line = document.createElementNS(SVG_NS, 'polyline');
+    coords.forEach(function(coord) {
+        allpoints.push([coord.x, coord.y].toString());
+    });
+    line.setAttribute('points', allpoints.join(" "));
+    line.style.fill = 'none';
+    return line;
+};
+
+var addTranslation = function(elem, dx, dy) {
+    elem.setAttribute('transform', 'translate(' + [dx, dy].toString() + ')');
+};
+
+var getTraceElement = function(profile, id, getCoordAtP) {
+    var g = createGroupElement(id);
+    if (!profile.isEmpty()) {
+        g.setAttribute('class', Elements.classes.TRACE);
+        var coords = [];
+        profile.pressures.forEach(function (pres) {
+            var coord = getCoordAtP(pres);
+            coords.push(coord);
+
+            var circ = document.createElementNS(SVG_NS, 'circle');
+            circ.setAttribute('cx', coord.x.toString());
+            circ.setAttribute('cy', coord.y.toString());
+            circ.setAttribute('r', '2');
+            circ.style.opacity = '0';
+            g.appendChild(circ);
+        });
+        var path = getPath(coords);
+        g.appendChild(path);
+    }
+    return g;
+};
---
+++ js/plot/plot-hodo.js
@@ -0,0 +1,125 @@
+/**
+ * Created by tangz on 4/28/2016.
+ */
+
+var HodographPlotter = (function (dim, hodoConfig, transform) {
+    var cx = dim.hodographArea.width / 2;
+    var cy = dim.hodographArea.height / 2;
+
+    var plotHodographBoundary = function (hodog) {
+        var rect = createBoundaryRect(Elements.HODO_BOUNDARY, dim.hodographArea.height, dim.hodographArea.width);
+        hodog.appendChild(rect);
+    };
+
+    function translateToCenter(elem) {
+        addTranslation(elem, cx, cy);
+    }
+
+    var plotHodographRadii = function (hodog) {
+        // may have to move this to plot-config
+        var rMax = Math.sqrt(cx*cx + cy*cy);
+
+        var hodoRadii = createGroupElement(Elements.WINDSPEED_RADII);
+        translateToCenter(hodoRadii);
+        hodog.appendChild(hodoRadii);
+
+        hodoConfig.radii.forEach(function (v) {
+            var r = rMax * v / hodoConfig.vMax;
+            var circ = document.createElementNS(SVG_NS, 'circle');
+            circ.setAttribute('cx', '0');
+            circ.setAttribute('cy', '0');
+            circ.setAttribute('r', r);
+            hodoRadii.appendChild(circ);
+        });
+    };
+
+    var plotHodographAxes = function (hodog) {
+        var g = createGroupElement(Elements.HODO_AXES);
+        hodog.appendChild(g);
+        translateToCenter(g);
+
+        var yAxisPts = [
+            {x:0, y:cy}, {x:0, y:-cy}
+        ];
+        var xAxisPts = [
+            {x:-cx, y:0}, {x:cx, y:0}
+        ];
+        var yAxis = getPath(yAxisPts);
+        var xAxis = getPath(xAxisPts);
+        g.appendChild(yAxis);
+        g.appendChild(xAxis);
+    };
+
+    var plotWindTrace = function(hodog) {
+        var filteredProfile = saved.soundingProfiles().filter(function (ob) {
+            return ob.hasHeight() && ob.hasWind();
+        });
+
+        var toHodoCoord = function (p) {
+            var windPt = filteredProfile.getValue(p);
+            var windDir = windPt.windDir();
+            var windSpd = windPt.windSpeed();
+            return transform.toHodographCoord(windSpd, windDir);
+        };
+
+        // find 3km, 6km, 9km pivot points
+        var threeKm, sixKm, nineKm;
+        var itr = filteredProfile.iterator();
+        while (itr.hasNext() && (threeKm = itr.next()).height() < 3000);
+        while (itr.hasNext() && (sixKm = itr.next()).height() < 6000);
+        while (itr.hasNext() && (nineKm = itr.next()).height() < 9000);
+
+        var elem03km, elem36km, elem69km, elemGT9km;
+
+        if (threeKm != null) {
+            elem03km = getTraceElement(filteredProfile.filter(function (pt) {
+                return pt.height() > 0 && pt.height() <= threeKm.height();
+            }), Elements.hodoTrace.KM_0_3, toHodoCoord);
+            elem03km.setAttribute('class', [elem03km.getAttribute('class'), Elements.classes.KM_0_3].join(" "));
+        }
+
+        if (threeKm != null && sixKm != null) {
+            elem36km = getTraceElement(filteredProfile.filter(function (pt) {
+                return pt.height() >= threeKm.height() && pt.height() <= sixKm.height();
+            }), Elements.hodoTrace.KM_3_6, toHodoCoord);
+            elem36km.setAttribute('class', [elem36km.getAttribute('class'), Elements.classes.KM_3_6].join(" "));
+        }
+
+        if (sixKm != null && nineKm != null) {
+            elem69km = getTraceElement(filteredProfile.filter(function (pt) {
+                return pt.height() >= sixKm.height() && pt.height() <= nineKm.height();
+            }), Elements.hodoTrace.KM_6_9, toHodoCoord);
+            elem69km.setAttribute('class', [elem69km.getAttribute('class'), Elements.classes.KM_6_9].join(" "));
+        }
+
+        if (nineKm != null) {
+            elemGT9km = getTraceElement(filteredProfile.filter(function (pt) {
+                return pt.height() >= nineKm.height();
+            }), Elements.hodoTrace.KM_gt_9, toHodoCoord);
+            elemGT9km.setAttribute('class', [elemGT9km.getAttribute('class'), Elements.classes.KM_gt_9].join(" "));
+        }
+
+        [elem03km, elem36km, elem69km, elemGT9km].forEach(function (elem) {
+            if (elem != null) {
+                translateToCenter(elem);
+                hodog.appendChild(elem);
+            }
+        });
+    };
+
+    return {
+        plotHodographOutline: function (hodographCanvas) {
+            hodographCanvas.setAttribute('x', dim.hodographArea.x.toString());
+            hodographCanvas.setAttribute('y', dim.hodographArea.y.toString());
+            hodographCanvas.setAttribute('width', dim.hodographArea.width.toString());
+            hodographCanvas.setAttribute('height', dim.hodographArea.height.toString());
+            plotHodographBoundary(hodographCanvas);
+            plotHodographRadii(hodographCanvas);
+            plotHodographAxes(hodographCanvas);
+        },
+
+        plotSoundingData: function(hodographCanvas) {
+            plotWindTrace(hodographCanvas);
+        }
+    }
+})(Dimensions, HodographPlotConfig, Transformer);
